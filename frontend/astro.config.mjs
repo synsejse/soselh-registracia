f1d96bbdf6d0d93fcad33004ab204ebabb9905ba
Dexxo <realdexxo@yahoo.com>
2025-12-09 14:21:36 +0100

    Clean-up frontend

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,12 +2,4 @@
 import {defineConfig} from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-    i18n: {
-        locales: ["sk"],
-        defaultLocale: "sk",
-    },
-    experimental: {
-        svgo: true,
-    }
-});
+export default defineConfig({});
